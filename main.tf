6b254273dd748c126769f8e75de871a61793dc6c
yuliiapnivchuk <yuliyapnivchuk@gmail.com>
2025-05-13 13:00:02 +0200

    fixes

--- main.tf
+++ main.tf
@@ -14,14 +14,6 @@ resource "azurerm_cognitive_account" "form_recognizer" {
   kind                = "FormRecognizer"
 }
 
-resource "azurerm_cognitive_account" "speech_recognizer" {
-  name                = "SpeechRecognizer-${random_string.azurerm_name.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = var.sku
-  kind                = "SpeechServices"
-}
-
 resource "azurerm_storage_account" "blob_storage" {
   name                     = "blobstorage${random_string.azurerm_name.result}"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -52,18 +44,6 @@ resource "azurerm_key_vault_secret" "form_recognizer_key" {
 resource "azurerm_key_vault_secret" "form_recognizer_endpoint" {
   name         = "DOC-INTELLIGENCE-ENDPOINT"
   value        = azurerm_cognitive_account.form_recognizer.endpoint
-  key_vault_id = data.azurerm_key_vault.existing.id
-}
-
-resource "azurerm_key_vault_secret" "speech_recognizer_key" {
-  name         = "SPEECH-RECOGNIZER-KEY"
-  value        = azurerm_cognitive_account.speech_recognizer.primary_access_key
-  key_vault_id = data.azurerm_key_vault.existing.id
-}
-
-resource "azurerm_key_vault_secret" "speech_recognizer_region" {
-  name         = "SPEECH-RECOGNIZER-REGION"
-  value        = azurerm_cognitive_account.speech_recognizer.location
   key_vault_id = data.azurerm_key_vault.existing.id
 }
 
